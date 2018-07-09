2e57f95fa8913cbed93415c64b91df4ff197f0b2
nestor_aparicio <nextore28@gmail.com>
2018-07-09 15:02:50 -0500

    Prueba login

---
+++ Lands/Lands/App.xaml.cs
@@ -0,0 +1,36 @@
+namespace Lands
+{
+    using System;
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+    using Views;
+
+    public partial class App : Application
+	{
+        #region Constructors
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new LoginPage());
+        }
+        #endregion
+
+        #region Methods
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        } 
+        #endregion
+    }
+}
